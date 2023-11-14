a5c355e333f2840e4e49a5b28e7f35118410abae
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-11-14 11:28:34 +0200

    Update PenSettingsForm.cs

--- PenSettingsForm.cs
+++ PenSettingsForm.cs
@@ -13,18 +13,22 @@ namespace LaborProject
 {
     public partial class PenSettingsForm : Form
     {
-        private Pen selectedPen;
+        public Pen SelectedPen { get; private set; }
+        public Color SelectedColor { get; private set; }
 
         private ComboBox styleComboBox;
+        private TrackBar widthTrackBar;
+        private Button applyButton;
+        private ColorDialog colorDialog;
 
         public PenSettingsForm(Pen currentPen)
         {
             InitializeComponent();
 
-            this.Height = 120;
+            this.Height = 180;
             this.Width = 260;
 
-            selectedPen = currentPen;
+            SelectedPen = currentPen;
 
             Label styleLabel = new Label();
             styleLabel.Text = "Style:";
@@ -40,30 +44,45 @@ namespace LaborProject
             styleComboBox.Location = new Point(120, 10);
             Controls.Add(styleComboBox);
 
-            Button applyButton = new Button();
+            Label widthLabel = new Label();
+            widthLabel.Text = "Width:";
+            widthLabel.Location = new Point(10, 40);
+            Controls.Add(widthLabel);
+
+            widthTrackBar = new TrackBar();
+            widthTrackBar.Minimum = 1;
+            widthTrackBar.Maximum = 20;
+            widthTrackBar.Value = (int)currentPen.Width;
+            widthTrackBar.LargeChange = 1;
+            widthTrackBar.Location = new Point(120, 40);
+            Controls.Add(widthTrackBar);
+
+            colorDialog = new ColorDialog();
+
+            applyButton = new Button();
             applyButton.Text = "Apply";
-            applyButton.Location = new Point(10, 40);
+            applyButton.Location = new Point(10, 70);
             applyButton.Click += ApplyButton_Click;
             Controls.Add(applyButton);
-
         }
+
         private void ApplyButton_Click(object sender, EventArgs e)
         {
-            selectedPen.Color = Color.Black;
-            selectedPen.Width = 2;
-
             if (styleComboBox.SelectedItem != null)
             {
-                selectedPen.DashStyle = (DashStyle)Enum.Parse(typeof(DashStyle), styleComboBox.SelectedItem.ToString());
+                SelectedPen.DashStyle = (DashStyle)Enum.Parse(typeof(DashStyle), styleComboBox.SelectedItem.ToString());
+            }
+
+            SelectedPen.Width = widthTrackBar.Value;
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                SelectedColor = colorDialog.Color;
             }
 
             DialogResult = DialogResult.OK;
             Close();
         }
-
-        public Pen SelectedPen
-        {
-            get { return selectedPen; }
-        }
     }
+
 }
